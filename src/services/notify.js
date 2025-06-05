3d09f8320cae9a234012d44d6d3e458461cda3c9
Weever <weever1337@gmail.com>
2025-06-05 21:48:08 +0300

    Fixing problem with send notifications (dont have permissions)

--- src/services/notify.js
+++ src/services/notify.js
@@ -4,15 +4,21 @@ import {formatMessage} from '../utils/helpers.js';
 
 export const sendNotification = async (event, payload) => {
     const repo = payload.repository.full_name.toLowerCase();
+    console.log(repo);
     const subscriptions = await Subscription.find({
         repository: {$regex: `^${repo}$`, $options: 'i'}
     });
+    console.log(subscriptions);
 
     for (const sub of subscriptions) {
         const message = formatMessage(event, payload);
-        await bot.telegram.sendMessage(sub.chatId, message, {
-            parse_mode: 'HTML',
-            disable_web_page_preview: true
-        });
+        try {
+            await bot.telegram.sendMessage(sub.chatId, message, {
+                parse_mode: 'HTML',
+                disable_web_page_preview: true
+            });
+        } catch {
+            console.log("Can't send message to: " + sub.chatId)
+        }
     }
 };
